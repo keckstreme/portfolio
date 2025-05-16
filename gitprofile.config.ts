22b85d7ee773fc6d03998fc5fc718bccbae5f528
keckstreme <108932848+keckstreme@users.noreply.github.com>
2025-05-16 07:59:30 +0300

    projects update

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -33,20 +33,87 @@ const CONFIG = {
             // To hide the `External Projects` section, keep it empty.
             projects: [
                 {
+                    title: 'Topic Word Link (released 2025)',
+                    description:
+                        'My first puzzle game under Finite Game Studio Ltd. It’s a word connect game with many minigames. Main gameplay is a word connect puzzle game.',
+                    imageUrl:
+                        '',
+                    link: 'https://play.google.com/store/apps/details?id=com.FiniteGameStudio.TopicWordLinkFGS&hl=en',
+                },
+                {
                     title: 'DriveUp (released 2021 & released 2025)',
                     description:
-                        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
+                        `Released in 2021, it was my first legitimate complete game. Later in 2025, I remastered it and published it again. It’s a 2D hill climbing 
+                        racing game. No third party assets were used, even in the remastered version.`,
                     imageUrl:
-                        'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
+                        'https://raw.githubusercontent.com/keckstreme/portfolio/main/stuff/RESUMEmedia/DriveUp/iconm.png',
                     link: 'https://play.google.com/store/apps/details?id=com.BurakKeklik.DriveUp',
                 },
                 {
-                    title: 'Project Name',
+                    title: '“Tofas Bursa Fabrikasi” (2024)',
                     description:
-                        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
+                        `Players can create as many cars as they want without any limitations and drive those cars in world maps. Players are able to save, load, 
+                        duplicate, and rename their cars. Pinch to zoom map and camera. Offering advanced controls and freedom.`,
                     imageUrl:
-                        'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
-                    link: 'https://example.com',
+                        '',
+                    link: 'https://www.youtube.com/watch?v=rJKiJ5CFUgg',
+                },
+                {
+                    title: 'Mech Survivors (2023)',
+                    description:
+                        `This was a child of Vampire Survivors trend. It was far from finished, but had a cool UI and 3D models that I created so I wanted to feature 
+                        it here.`,
+                    imageUrl:
+                        '',
+                    link: 'https://www.youtube.com/watch?v=bBl05aa48qM',
+                },
+                {
+                    title: 'driving tech (2022)',
+                    description:
+                        `Realistic arcade car physics`,
+                    imageUrl:
+                        '',
+                    link: 'https://www.youtube.com/watch?v=VH04U8l49HU',
+                },
+                {
+                    title: 'Charge It All (2022)',
+                    description:
+                        `Charge incoming phones by selecting empty and correct charging plugs.`,
+                    imageUrl:
+                        '',
+                    link: '',
+                },
+                {
+                    title: 'Zombie Defense (2022)',
+                    description:
+                        `I wanted to create a game that I would play, therefore Zombie Defense was born. It’s a base defender shooter game.`,
+                    imageUrl:
+                        '',
+                    link: '',
+                },
+                {
+                    title: 'Shift Race (2022)',
+                    description:
+                        `My second 3D hyper-casual game under Lecten. It's a drag racing game with obstacles. Player needs to modify their car in real time to avoid losing speed.`,
+                    imageUrl:
+                        '',
+                    link: '',
+                },
+                {
+                    title: 'Toony Race (2022)',
+                    description:
+                        `My first hyper-casual and 3D game under the umbrella of Lecten. It's a level based racing game with Mario Kart mechanics.`,
+                    imageUrl:
+                        '',
+                    link: '',
+                },
+                {
+                    title: 'Incremental Jump (2021)',
+                    description:
+                        `Released in 2021, it was my second game after DriveUp. It was a Doodle Jump with incremental score features. Incremental Jump was mainly an experiment/exercise. `,
+                    imageUrl:
+                        '',
+                    link: 'https://www.youtube.com/watch?v=ojJYCIj-NEk',
                 },
             ],
         },
@@ -80,7 +147,7 @@ const CONFIG = {
     },
     resume: {
         fileUrl:
-            'https://raw.githubusercontent.com/keckstreme/portfolio/stuff/RESUME.pdf', // Empty fileUrl will hide the `Download Resume` button.
+            'https://raw.githubusercontent.com/keckstreme/portfolio/main/stuff/RESUME.pdf', // Empty fileUrl will hide the `Download Resume` button.
     },
     skills: [
         'Unity',
@@ -238,7 +305,7 @@ const CONFIG = {
     },
 
     // Optional Footer. Supports plain text or HTML.
-    footer: '',//`Made with <a class="text-primary" href="https://github.com/arifszn/gitprofile" target="_blank" rel="noreferrer">GitProfile</a> and ❤️`,
+    footer: 'dsfsdf',//`Made with <a class="text-primary" href="https://github.com/arifszn/gitprofile" target="_blank" rel="noreferrer">GitProfile</a> and ❤️`,
 
     enablePWA: false, // GitProfile is PWA enabled. The site can be installed as a Progressive Web App. To turn it off, set enablePWA to false.
 };
